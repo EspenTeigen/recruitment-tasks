05d2408f547b296be9f278a0a340870afdf162e9
EspenTeigen <espen.teigen@hotmail,com>
2022-06-15 20:19:33 +0200

    Cleaned up code and comments

--- temperature-sensor/main.c
+++ temperature-sensor/main.c
@@ -9,9 +9,12 @@
 #include <math.h>
 #include <curl/curl.h>
 
-#define MAX_JSON_STRING_SIZE 150
-#define N_OLD_JSON_STRINGS 10
+#define MAX_JSON_STRING_SIZE 150     
+#define N_OLD_JSON_STRINGS 10       //Maximum of failed JSON-strings
+#define N_ANALOG_VALUES 766         //Lines of values in temperature.txt
+#define INTERVAL_TO_POST_REQUEST_SEC 120 //Time in seconds between POST requests
 
+//Where all values related to ADC is stored
 typedef struct TemperatureMeasurement{
         char start[80];
         int sizeOfStart;       
@@ -24,14 +27,12 @@ typedef struct TemperatureMeasurement{
         int number_of_measurements;
     }temperatureMeasurement;
 
-
-
 //-----------ADC related variables and functions-----------------
 
-//Lines of values in temperature.txt
-#define N_ANALOG_VALUES 766
+
 //Sleep value in thread readADCTimer to sample data every 100ms(100 000us)
 const float ADC_SAMPLE_FREQUENCY =  100000;
+const int NUMBER_OF_SAMPLES_IN_INTERVAL = (ADC_SAMPLE_FREQUENCY / 10000) * INTERVAL_TO_POST_REQUEST_SEC; // How many samples there should be between POST request
 
 
 //Used to store all the values from file to simulate getting data from
@@ -40,40 +41,31 @@ uint16_t adc_values_from_file[N_ANALOG_VALUES];
 uint8_t adc_ready = 0x00;
 uint16_t single_value_from_adc;
 
-
-//used to lock thread so values cannot be changed during data retrieval
-static pthread_mutex_t adc_lock;
-//Start thread and mutex lock
-int initReadADCTimer(char * filename);
-//samples single value from array of samples every 100ms
-void *readADCTimer(void *vargp);
+static pthread_mutex_t adc_lock; //used to lock thread so values cannot be changed during data retrieval
+int initReadADCTimer(char * filename);  //Start thread and mutex lock
+void *readADCTimer(void *vargp); //samples single value from array of samples every 100ms
 void convertToCelsius(temperatureMeasurement * measurement);
-//Read adc values from temperature.txt and stores it in adc_values_from_file
-void getAllADCTemperatureFromFile(char * filename);
+void getAllADCTemperatureFromFile(char * filename); //Read adc values from temperature.txt and stores it in adc_values_from_file
 
 
 //----------------POST request variables and functions-----------------
 
-//Sleep value in thread pubToPOST to send data every two minutes
-const int post_adc_data_frequency = 3;
+
 //Toggled by pubToPOST thread every two minutes to signal that is
 //time to send data
 uint8_t sendDataPOST = 0x00;
-//Lock during work on shared memory in thread
-static pthread_mutex_t post_lock;
+static pthread_mutex_t post_lock; //Lock during work on shared memory in thread
 
 
-int initpubToPOSTTimer();
-void *pubToPOST(void * vargp);
+int initpubToPOSTTimer(); //Starts thread and mutex lock (probably overkill for this)
+void *pubToPOST(void * vargp); //Thread that triggers the sendDataPOST flag every two minuts
 void createJSON(temperatureMeasurement * measurement, char * string, int size_of_string);
 void getDateTimeISO8601(char * dateTime, int sizeDateTime);
-int POSTMeasurement(const char * URL, char * measurement_json);
+int POSTMeasurement(const char * URL, char * measurement_json); //Function that uses libcurl to create POST request and read error from server
 
-//used to store previous json strings that failed to be sent
-typedef struct PreviousJsonString{
-    //json string
-    char previous_json_string[N_OLD_JSON_STRINGS][MAX_JSON_STRING_SIZE];
-    //number of elements inn array
+
+typedef struct PreviousJsonString{//used to store previous json strings that failed to be sent, number of errors and elementrs is stored
+    char previous_json_string[N_OLD_JSON_STRINGS][MAX_JSON_STRING_SIZE]; 
     uint8_t N_errors;
     uint8_t N_elements;
 }previousJsonString;
@@ -85,7 +77,7 @@ typedef struct PreviousJsonString{
 int main()
 {
     //----------------Initialization-------------
-    //Variable to store ADC data
+    //Initialize struct to store ADC data
     temperatureMeasurement tempMeas;
     tempMeas.number_of_measurements = 0;
     tempMeas.sizeOfStart = sizeof(tempMeas.start);
@@ -102,42 +94,36 @@ int main()
     //------------Main loop-----------------------------
 
     while(1){
+
         //Get data everytime adc is ready
         if(adc_ready){
             convertToCelsius(&tempMeas);
             adc_ready = 0x00;
         }
 
-        //Check if ready to post and that there is enough samples to give a propper average
-        if(sendDataPOST && tempMeas.number_of_measurements >= 10){
+        //Check if ready to post and that there is enough samples that the
+        //average is similar even when we boot up the system
+        if(sendDataPOST && tempMeas.number_of_measurements >= NUMBER_OF_SAMPLES_IN_INTERVAL){
+
+            sendDataPOST = 0x00; //Reset flag that signals it is time to send data
             
-            //keep track on failing to send
-            static uint8_t errSend = 0x00;
-            //Store local copy so things dont change before data is sent
-            temperatureMeasurement tm_copy = tempMeas;
+            temperatureMeasurement tm_copy = tempMeas; //Store local copy so things dont change before data is sent (this is overkill for a slow system ) 
 
-            //reset number_of_measurements so everything will be reset in convertToCelsius()
-            tempMeas.number_of_measurements = 0;
+            tempMeas.number_of_measurements = 0; //reset number_of_measurements so everything will be reset in convertToCelsius()
 
-            //Store end time
             getDateTimeISO8601(tm_copy.end, tm_copy.sizeOfEnd);
 
-            //calculate average
-            tm_copy.average = tm_copy.sum / tm_copy.number_of_measurements;
-
-            //Reset flag that signals it is time to send data
-            sendDataPOST = 0x00;
-
+            tm_copy.average = tm_copy.sum / tm_copy.number_of_measurements; //calculate average of temperature
+            
             //construct json
-            char measurement_json[MAX_JSON_STRING_SIZE];
+            char measurement_json[MAX_JSON_STRING_SIZE];  
             createJSON(&tm_copy, measurement_json, MAX_JSON_STRING_SIZE);
         
             //This could probably be done more elegant by putting it in a textfile
             static const char * POST_URL = "http://localhost:5000/api/temperature";
             static const char * FALLBACK_URL = "http://localhost:5000/api/temperature/missing";            
-
             
-            //Send data to , if it fails, store value and send on the next 
+            //Send data, if it fails, store value and send on the next 
             //two minute interval
             if(POSTMeasurement(POST_URL ,measurement_json)){
                //start to fill up array with old values, increment amount of elements in array       
@@ -148,21 +134,19 @@ int main()
 
             //When POST request has failed 10 times, send data to /missing, and empty array
             if(failedAttempts.N_elements >= 10) {
-                char failed_measurement_combined[10*MAX_JSON_STRING_SIZE] = {"\0, \0, \0, \0, \0, \0, \0, \0, \0, \0, "};
+
+                char failed_measurement_combined[10*MAX_JSON_STRING_SIZE];
                 int i;
                 //Combine all failed attempts
                 for(i = 0; i < failedAttempts.N_elements; i++){
-                    //Concatenate all strings in array
+        
                     strcat(failed_measurement_combined, failedAttempts.previous_json_string[i]);
                     strcat(failed_measurement_combined, "\n");
-                    //Empty the array space
-                    strcpy(failedAttempts.previous_json_string[i], "\0");
+    
                 }
 
-                //Since strings in array is removed, set counter to zero
-                failedAttempts.N_elements = 0;
-                //Send combined string to fallback server
-                POSTMeasurement(FALLBACK_URL, failed_measurement_combined);
+                failedAttempts.N_elements = 0;  //Since strings in array is removed, set counter to zero
+                POSTMeasurement(FALLBACK_URL, failed_measurement_combined);  //Send combined string to fallback server
                 
             }
         }
@@ -175,8 +159,8 @@ int main()
 void createJSON(temperatureMeasurement * measurement, char * string, int size_of_string){
     
     char buffer[size_of_string];
-    //Format data to json raw string, probably more elegant to keep json string in textfile and
-    //and use a json library to parse it, but that is not a v ery embedded thing to do
+    //Format data to json raw string, probably more elegant to keep json string in textfile
+    //and use a json library to parse it, but that is not a very embedded thing to do
     sprintf(buffer, "{\n\"time\":{\n\t\"start\" : \"%s\", \n\t\"end\" : \"%s\" \n\t},\n	\"min\" : %.2f, \n\t\"max\" : %.2f,\t\n	\"avg\" : %.2f\n}",
             measurement->start, measurement->end, measurement->min, measurement->max, measurement->average);
 
@@ -246,11 +230,11 @@ void *readADCTimer(void *filename) {
 
     //Runs through all samples and roll over to beginning when end is reach
     //length of array is hardcoded since this will not change
-    //In a situation I would use an interrupt vector to detect when ADC is ready
+    //In a real situation I would use an interrupt vector to detect when ADC is ready
     while(1){
         //The adc_ready_flag is used to reduce time used in thread, and improve determinism of
         //sample collection time since time used to convert data does not add to the cycle time
-        //with this method
+        //with this method. This is overkill for this system
         if(i < N_ANALOG_VALUES) {
 
             pthread_mutex_lock(&adc_lock);
@@ -274,8 +258,7 @@ void *readADCTimer(void *filename) {
 
 void convertToCelsius(temperatureMeasurement * measurement){
 
-    //Conversion from 12-bit adc to celsius when f(0)=-50 and f(4096)=50. f(x) = 0.0244x - 50 
-    float temperature = 0.0244*(float)single_value_from_adc - 50.0;
+    float temperature = 0.0244*(float)single_value_from_adc - 50.0;  //Conversion from 12-bit adc to celsius when f(0)=-50 and f(4096)=50. f(x) = 0.0244x - 50 
 
     //Reset all values when data has been read, number_of_measurements are reset 
     //when post is triggered
@@ -284,13 +267,13 @@ void convertToCelsius(temperatureMeasurement * measurement){
         measurement->average = 0;
         measurement->max = temperature;
         measurement->min = temperature;
-        //Save dateTime from when we start to sample data
-        getDateTimeISO8601(measurement->start, measurement->sizeOfStart);
+        getDateTimeISO8601(measurement->start, measurement->sizeOfStart); //Save dateTime from when we start to sample data
     }
     
     //sum all values and amount of measurements, used to calculate average
     measurement->sum += temperature;
     measurement->number_of_measurements++;
+
     //Find max and min values
     if(temperature > measurement->max) measurement->max = temperature;
     if(temperature < measurement->min) measurement->min = temperature;
@@ -306,7 +289,7 @@ void *pubToPOST(void *vargp){
         pthread_mutex_lock(&post_lock);
         //Rais flag to tell if test in main to post data
         sendDataPOST = 0x01;
-        sleep(post_adc_data_frequency);
+        sleep(INTERVAL_TO_POST_REQUEST_SEC);
         pthread_mutex_unlock(&post_lock);
     }
 }
@@ -332,27 +315,19 @@ int POSTMeasurement(const char * URL, char * measurement_json){
     char error_buf[CURL_ERROR_SIZE];
     curl = curl_easy_init();
     if(curl) {
-        //tell curl that we want to post
-        curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "POST");
-        //URL to server and api
-        curl_easy_setopt(curl, CURLOPT_URL, URL);
-        //Turn on error feedback so we can catch the 500 error
-        curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1L);
-        curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, error_buf);
-
-        //curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-        curl_easy_setopt(curl, CURLOPT_DEFAULT_PROTOCOL, "https");
+        curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "POST"); //tell curl that we want to post
+        curl_easy_setopt(curl, CURLOPT_URL, URL);  //URL to server and api
+        curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1L);  //Turn on error feedback so we can catch the 500 error
+        curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, error_buf); //Get error from server
+        curl_easy_setopt(curl, CURLOPT_DEFAULT_PROTOCOL, "https"); //We want to use http protocol
 
         //Include json header
         struct curl_slist *headers = NULL;
         headers = curl_slist_append(headers, "Content-Type: application/json");
         curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
-        //json string to be sent over https
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, measurement_json);
-        //Send data 
-        res = curl_easy_perform(curl);
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, measurement_json); //json string to be sent over https
+        res = curl_easy_perform(curl); //Send data and store response from server
 
-    
         ///if it contains error 500, send err that fallback server needs to be used in next itteration
         //I do not check for other errors since i'm asked to only do error handling on 500
         if(strstr(error_buf, "500")){
